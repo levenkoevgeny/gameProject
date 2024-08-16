985d9639af48d71faa017c7c50ee78d8b30e288e
evgeny <levenko.evgeny@yandex.by>
2024-08-16 08:31:53 +0300

    fix Dockerfile

--- game/migrations/0014_auto_20240813_0913.py
+++ game/migrations/0014_auto_20240813_0913.py
@@ -76,140 +76,139 @@ def init_questions(apps, schema_editor):
 
     # Answers
 
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=1)),
+    Answer.objects.create(question=Question.objects.get(question_number=1),
                           answer_text="Вы проверяете является ли совершенное действие противоправным в соответствии с УК Республики Беларусь)",
                           score=2,
-                          next_question=Subquery(Question.objects.get(question_number=2)))
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=1)),
+                          next_question=Question.objects.get(question_number=2))
+    Answer.objects.create(question=Question.objects.get(question_number=1),
                           answer_text="Вы выезжаете для задержания лица сами вместе с коллегой из НЦБ Интерпола в Республике Беларусь",
                           score=1,
-                          next_question=Subquery(Question.objects.get(question_number=7)))
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=1)),
+                          next_question=Question.objects.get(question_number=7))
+    Answer.objects.create(question=Question.objects.get(question_number=1),
                           answer_text="Вы направляете уведомление в РУВД/РОВД по месту регистрации и проживания разыскиваемого лица",
                           score=2,
-                          next_question=Subquery(Question.objects.get(question_number=6)))
+                          next_question=Question.objects.get(question_number=6))
 
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=2)),
+    Answer.objects.create(question=Question.objects.get(question_number=2),
                           answer_text="Совершенные лицом действия в Республике Беларусь являются тяжким преступлением",
                           score=0,
-                          next_question=Subquery(Question.objects.get(question_number=3)))
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=2)),
+                          next_question=Question.objects.get(question_number=3))
+    Answer.objects.create(question=Question.objects.get(question_number=2),
                           answer_text="Совершенные лицом действия в Республике Беларусь являются менее тяжким преступлением",
                           score=0,
-                          next_question=Subquery(Question.objects.get(question_number=3)))
+                          next_question=Question.objects.get(question_number=3))
 
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=3)),
+    Answer.objects.create(question=Question.objects.get(question_number=3),
                           answer_text="необходимо осуществить задержание",
                           score=1,
-                          next_question=Subquery(Question.objects.get(question_number=5)))
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=3)),
+                          next_question=Question.objects.get(question_number=5))
+    Answer.objects.create(question=Question.objects.get(question_number=3),
+                          answer_text="не осуществлять задержание лица, разыскиваемого другим государством",
+                          score=-1)
+    Answer.objects.create(question=Question.objects.get(question_number=4),
+                          answer_text="необходимо осуществить задержание",
+                          score=1,
+                          next_question=Question.objects.get(question_number=5))
+    Answer.objects.create(question=Question.objects.get(question_number=4),
                           answer_text="не осуществлять задержание лица, разыскиваемого другим государством",
                           score=-1)
 
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=4)),
-                          answer_text="необходимо осуществить задержание",
-                          score=1,
-                          next_question=Subquery(Question.objects.get(question_number=5)))
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=4)),
-                          answer_text="не осуществлять задержание лица, разыскиваемого другим государством",
-                          score=-1)
-
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=5)),
+    Answer.objects.create(question=Question.objects.get(question_number=5),
                           answer_text="Вы выезжаете для задержания лица сами вместе с коллегой из НЦБ Интерпола в Республике Беларусь",
                           score=1,
-                          next_question=Subquery(Question.objects.get(question_number=7)))
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=5)),
+                          next_question=Question.objects.get(question_number=7))
+    Answer.objects.create(question=Question.objects.get(question_number=5),
                           answer_text="Вы направляете уведомление в РУВД/РОВД по месту регистрации и проживания разыскиваемого лица",
                           score=2,
-                          next_question=Subquery(Question.objects.get(question_number=6)))
+                          next_question=Question.objects.get(question_number=6))
 
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=6)),
+    Answer.objects.create(question=Question.objects.get(question_number=6),
                           answer_text="НАПРАВЛЯЕТ Сотрудников ОВД для задержания лица",
                           score=1,
-                          next_question=Subquery(Question.objects.get(question_number=7)))
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=6)),
+                          next_question=Question.objects.get(question_number=7))
+    Answer.objects.create(question=Question.objects.get(question_number=6),
                           answer_text="НЕ РЕАГИРОВАТЬ не регистрировать / не создавать материал проверки сообщения",
                           score=-2)
 
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=7)),
+    Answer.objects.create(question=Question.objects.get(question_number=7),
                           answer_text="ЗАДЕРЖАНИЕ в порядке ст. 510 УПК Республики Беларусь",
                           score=1,
-                          next_question=Subquery(Question.objects.get(question_number=8)))
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=7)),
+                          next_question=Question.objects.get(question_number=8))
+    Answer.objects.create(question=Question.objects.get(question_number=7),
                           answer_text="ЗАДЕРЖАНИЕ в порядке ст. 108 УПК Республики Беларусь",
                           score=-1)
 
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=8)),
+    Answer.objects.create(question=Question.objects.get(question_number=8),
                           answer_text="Вынесение постановления о задержании",
                           score=1)
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=8)),
+    Answer.objects.create(question=Question.objects.get(question_number=8),
                           answer_text="Передачу задержанного следователю РО СК",
                           score=-1)
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=8)),
+    Answer.objects.create(question=Question.objects.get(question_number=8),
                           answer_text="Опрос лица",
                           score=-1,
-                          next_question=Subquery(Question.objects.get(question_number=8)))
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=8)),
+                          next_question=Question.objects.get(question_number=8))
+    Answer.objects.create(question=Question.objects.get(question_number=8),
                           answer_text="Передачу задержанного прокурору района",
                           score=1,
-                          next_question=Subquery(Question.objects.get(question_number=9)))
+                          next_question=Question.objects.get(question_number=9))
 
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=9)),
+    Answer.objects.create(question=Question.objects.get(question_number=9),
                           answer_text="Об освобождении",
                           score=-1)
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=9)),
+    Answer.objects.create(question=Question.objects.get(question_number=9),
                           answer_text="О заключении под стражу",
                           score=1,
-                          next_question=Subquery(Question.objects.get(question_number=10)))
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=9)),
+                          next_question=Question.objects.get(question_number=10))
+    Answer.objects.create(question=Question.objects.get(question_number=9),
                           answer_text="О выдаче лица",
                           score=-1)
 
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=10)),
+    Answer.objects.create(question=Question.objects.get(question_number=10),
                           answer_text="10 суток",
                           score=-1)
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=10)),
+    Answer.objects.create(question=Question.objects.get(question_number=10),
                           answer_text="30 суток",
                           score=-1)
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=10)),
+    Answer.objects.create(question=Question.objects.get(question_number=10),
                           answer_text="40 суток",
                           score=1,
-                          next_question=Subquery(Question.objects.get(question_number=11)))
+                          next_question=Question.objects.get(question_number=11))
 
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=11)),
+    Answer.objects.create(question=Question.objects.get(question_number=11),
                           answer_text="Сообщает в Министерство иностранных дел Республики Беларусь о принятом решении",
                           score=1)
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=11)),
+    Answer.objects.create(question=Question.objects.get(question_number=11),
                           answer_text="Передает материал территориальному ОВД",
                           score=-1)
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=11)),
+    Answer.objects.create(question=Question.objects.get(question_number=11),
                           answer_text="Осуществляет экстрадиционную проверку",
                           score=1,
-                          next_question=Subquery(Question.objects.get(question_number=12)))
+                          next_question=Question.objects.get(question_number=12))
 
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=12)),
+    Answer.objects.create(question=Question.objects.get(question_number=12),
                           answer_text="Осуществляют проверку по материалу, зарегистрированному в ЕК, и ОРМ по установлению местонахождения лица",
                           score=1,
-                          next_question=Subquery(Question.objects.get(question_number=13)))
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=12)),
+                          next_question=Question.objects.get(question_number=13))
+    Answer.objects.create(question=Question.objects.get(question_number=12),
                           answer_text="Не осуществляют иных ОРМ и процессуальных действий",
                           score=-1)
 
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=13)),
+    Answer.objects.create(question=Question.objects.get(question_number=13),
                           answer_text="Нет",
                           score=-2)
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=13)),
+    Answer.objects.create(question=Question.objects.get(question_number=13),
                           answer_text="Да (постановлением начальника ОВД)",
                           score=-1)
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=13)),
+    Answer.objects.create(question=Question.objects.get(question_number=13),
                           answer_text="Да (рапортом сотрудника)",
                           score=1,
-                          next_question=Subquery(Question.objects.get(question_number=14)))
+                          next_question=Question.objects.get(question_number=14))
 
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=14)),
+    Answer.objects.create(question=Question.objects.get(question_number=14),
                           answer_text="Нет (неверно)",
                           score=-1)
-    Answer.objects.create(question=Subquery(Question.objects.get(question_number=13)),
+    Answer.objects.create(question=Question.objects.get(question_number=13),
                           answer_text="Да",
                           score=1)
 
